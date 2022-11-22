76ecbf3053d6d0c8193cb2b8f8a8a8e1ad939fdd
TralahM <briantralah@gmail.com>
2022-11-23 01:10:03 +0300

    error crc

--- crc.h
+++ crc.h
@@ -10,4 +10,7 @@
  *
  * =====================================================================================
  */
-unsigned int crc32(unsigned char* message);
+#include <stdlib.h>
+
+uint32_t calculate_crc32c(uint32_t crc32c, const unsigned char* buffer,
+                          unsigned int length);
